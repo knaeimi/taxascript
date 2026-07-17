81997e5003fc1f81b340c1be5a174361fed74e73
knaeimi <knaeimi@macalester.edu>
2026-07-17 10:57:12 -0500

    hm

---
+++ taxaAnalyzer.java
@@ -0,0 +1,22 @@
+
+public class taxaAnalyzer {
+
+private String fileDirectory;
+
+//Take in a file directory of taxa profiles, 
+
+public taxaAnalyzer(String fileDirectory){
+    this.fileDirectory = fileDirectory;
+}
+
+/**
+ * 
+ * @return # of taxa found in metaphlan text file
+ */
+public String taxaIdentified(){
+    
+
+    return "";
+}
+    
+}
