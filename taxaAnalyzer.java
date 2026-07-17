d7a8a070da58014b976db572bc885f177259284d
knaeimi <knaeimi@macalester.edu>
2026-07-17 13:55:24 -0500

    Finished. Testing now

--- taxaAnalyzer.java
+++ taxaAnalyzer.java
@@ -7,44 +7,45 @@ import java.nio.file.Paths;
 
 public class TaxaAnalyzer {
 
-private String directory;
+    private String directory;
 
-//Take in a file directory of taxa profiles, 
+    //Take in a file directory of taxa profiles, 
 
-public TaxaAnalyzer(String directory){
-    this.directory = directory;
-}
+    public TaxaAnalyzer(String directory){
+        this.directory = directory;
+    }
 
-/**
- * 
- * @return # of taxa found in metaphlan text file
- */
-public int countTaxa(){
-    int taxaCount = 0;
-    Path directoryPath = Paths.get(directory); //convert directory string to a concrete file path
-    File[] files = directoryPath.toFile().listFiles(); 
+    /**
+     * 
+     * @return # of taxa found in each metaphlan text file in the passed directory
+     */
+    public void countTaxa(){
+        Path directoryPath = Paths.get(directory); //convert directory string to a concrete file path
+        File[] files = directoryPath.toFile().listFiles(); 
 
-    for (File file : files) {
+        for (File file : files) {
 
-        if (!file.getName().endsWith("_profile.txt")) continue; //skip files in the directory that aren't metaphlan taxonomic profiles
+            int taxaCount = 0; //each file in the directory has its own taxa count, so reset after processing each one
 
-        System.out.println(file.getName()); //Valid profile, so lets print which one it is for debugging
+            if (!file.getName().endsWith("_profile.txt")) continue; //skip files in the directory that aren't metaphlan taxonomic profiles
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {//sytax here: Pass rescource in try and it will close the reader for you at the end. Syntactic sugar.
-            
-            String line = reader.readLine();
+            System.out.println(file.getName()); //Valid profile, so lets print which one it is for debugging
 
-            while (line != null) {
-                if (line.startsWith("#")) continue; //skip lines with comments
-                taxaCount++; //every non comment line in the text file is a new taxon
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {//sytax here: Pass rescource in try and it will close the reader for you at the end. Syntactic sugar.
+                
+                String line = reader.readLine();
+
+                while (line != null) {
+                    if (line.startsWith("#")) continue; //skip lines with comments
+                    taxaCount++; //every non comment line in the text file is a new taxon
+                }
+                System.out.println(file.getName() + "has a taxa count of: " + taxaCount);
+            } 
+            catch (IOException e) { //try-catch syntax for debugging
+                e.printStackTrace();
             }
-        } 
-        catch (IOException e) { //try-catch syntax for debugging
-            e.printStackTrace();
         }
     }
 
-    return taxaCount; //This only makes sense for one file right now. Figure out how to print out a taxa count for each file in the directory.
-}
     
 }
